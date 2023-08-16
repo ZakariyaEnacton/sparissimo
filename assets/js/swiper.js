612c48887939b831b2cdd36a4513567ca44aba39
ZakariyaEnacton <mzb.enacton@gmail.com>
2023-08-16 18:47:39 +0530

    card section done

---
+++ assets/js/swiper.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".banner-swwiper", {
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  navigation: {
+    nextEl: ".hero-button-next",
+    prevEl: ".hero-button-prev",
+  },
+});
