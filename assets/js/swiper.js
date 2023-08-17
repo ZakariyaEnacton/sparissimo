65453aca11d43ceae93219da3bc9928eafc9a160
ZakariyaEnacton <mzb.enacton@gmail.com>
2023-08-17 17:03:36 +0530

    top deals section done

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -17,3 +17,26 @@ var swiper = new Swiper(".card-swiper", {
     prevEl: ".card-button-prev",
   },
 });
+
+var swiper = new Swiper(".grid-card-swiper", {
+  slidesPerView: 2,
+  slidesPerColumn: 2,
+  grid: {
+    row: 2,
+    fill: "row",
+  },
+  spaceBetween: 30,
+  // navigation: {
+  //   nextEl: ".card-button-next",
+  //   prevEl: ".card-button-prev",
+  // },
+});
+
+var swiper = new Swiper(".deals-swiper", {
+  spaceBetween: 30,
+  slidesPerView: 5,
+  navigation: {
+    nextEl: ".d-card-button-next",
+    prevEl: ".d-card-button-prev",
+  },
+});
