f6162419120a0efce66ddc1aaec1a5f1b1335bfd
ZakariyaEnacton <mzb.enacton@gmail.com>
2023-08-17 11:35:17 +0530

    header customization

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -8,3 +8,12 @@ var swiper = new Swiper(".banner-swwiper", {
     prevEl: ".hero-button-prev",
   },
 });
+
+var swiper = new Swiper(".card-swiper", {
+  spaceBetween: 30,
+  slidesPerView: 5,
+  navigation: {
+    nextEl: ".card-button-next",
+    prevEl: ".card-button-prev",
+  },
+});
